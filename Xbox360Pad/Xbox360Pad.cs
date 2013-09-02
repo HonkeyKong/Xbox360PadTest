3240153a43733ed8a9c3b375e78b7ffde1e28011
Ryan D. Souders <honkeykong@honkeykong.org>
2013-09-02 17:27:37 -0500

    Added analog stick support, updated test app.

---
+++ Xbox360Pad/Xbox360Pad.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.XInput;
+
+namespace Xbox360Pad
+{
+    public sealed class Pad
+    {
+
+        //public Controller csPad = new Controller(0);
+
+        public static bool isControllerConnected(int index)
+        {
+            Controller csPad = new Controller((UserIndex)index);
+            return csPad.IsConnected;
+        }
+
+        public static bool isPressed(int index, string button)
+        {
+            Controller csPad = new Controller((UserIndex)index);
+            int bFlag = 0;
+            if (csPad.IsConnected)
+            {
+                switch (button)
+                {
+                    case "A":
+                        bFlag = 0x1000;
+                        break;
+                    case "B":
+                        bFlag = 0x2000;
+                        break;
+                    case "X":
+                        bFlag = 0x4000;
+                        break;
+                    case "Y":
+                        bFlag = 0x8000;
+                        break;
+                    case "DPadUp":
+                        bFlag = 0x1;
+                        break;
+                    case "DPadDown":
+                        bFlag = 0x2;
+                        break;
+                    case "DPadLeft":
+                        bFlag = 0x4;
+                        break;
+                    case "DPadRight":
+                        bFlag = 0x8;
+                        break;
+                    case "Start":
+                        bFlag = 0x10;
+                        break;
+                    case "Back":
+                        bFlag = 0x20;
+                        break;
+                    case "LeftClick":
+                        bFlag = 0x40;
+                        break;
+                    case "RightClick":
+                        bFlag = 0x80;
+                        break;
+                    case "LeftBumper":
+                        bFlag = 0x100;
+                        break;
+                    case "RightBumper":
+                        bFlag = 0x200;
+                        break;
+                }
+                State csState = csPad.GetState();
+                GamepadButtonFlags flags = csState.Gamepad.Buttons;
+                if (((int)flags & bFlag) >= 1)
+                    return true;
+                else return false;
+            }
+            else return false;
+        }
+
+        public static byte getLeftTrigger(int index)
+        {
+            Controller csPad = new Controller((UserIndex)index);
+            State csState = csPad.GetState();
+            return csState.Gamepad.LeftTrigger;
+        }
+
+        public static byte getRightTrigger(int index)
+        {
+            Controller csPad = new Controller((UserIndex)index);
+            State csState = csPad.GetState();
+            return csState.Gamepad.RightTrigger;
+        }
+
+        public static short getLStickX(int index)
+        {
+            Controller csPad = new Controller((UserIndex)index);
+            State csState = csPad.GetState();
+            return csState.Gamepad.LeftThumbX;
+        }
+
+        public static short getLStickY(int index)
+        {
+            Controller csPad = new Controller((UserIndex)index);
+            State csState = csPad.GetState();
+            return csState.Gamepad.LeftThumbY;
+        }
+
+        public static short getRStickX(int index)
+        {
+            Controller csPad = new Controller((UserIndex)index);
+            State csState = csPad.GetState();
+            return csState.Gamepad.RightThumbX;
+        }
+
+        public static short getRStickY(int index)
+        {
+            Controller csPad = new Controller((UserIndex)index);
+            State csState = csPad.GetState();
+            return csState.Gamepad.RightThumbY;
+        }
+
+    }
+}
---
+++ Xbox360PadTest/js/default.js
@@ -0,0 +1,122 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+
+                if (pad.isControllerConnected(0)) document.getElementById("padStatus").innerText = "Xbox 360 Controller detected!";
+                else document.getElementById("padStatus").innerText = "No controller detected.";
+
+                
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
+})();
+
+var pad = Xbox360Pad.Pad;
+
+function isConnected(index) {
+    return pad.isControllerConnected();
+}
+
+function getLT(index) {
+    document.getElementById("LTResult").innerText = pad.getLeftTrigger(0);
+}
+
+function getRT(index) {
+    document.getElementById("RTResult").innerText = pad.getRightTrigger(0);
+}
+
+function getLSX(index) {
+    document.getElementById("LStickXResult").innerText = pad.getLStickX(0);
+}
+
+function getLSY(index) {
+    document.getElementById("LStickYResult").innerText = pad.getLStickY(0);
+}
+
+function getRSX(index) {
+    document.getElementById("RStickXResult").innerText = pad.getRStickX(0);
+}
+
+function getRSY(index) {
+    document.getElementById("RStickYResult").innerText = pad.getRStickY(0);
+}
+
+
+function btnPressed(index, button) {
+    switch (button) {
+        case "A":
+            document.getElementById("ButtonAResult").innerText = pad.isPressed(index, button);
+            break;
+        case "B":
+            document.getElementById("ButtonBResult").innerText = pad.isPressed(index, button);
+            break;
+        case "X":
+            document.getElementById("ButtonXResult").innerText = pad.isPressed(index, button);
+            break;
+        case "Y":
+            document.getElementById("ButtonYResult").innerText = pad.isPressed(index, button);
+            break;
+        case "Start":
+            document.getElementById("ButtonStartResult").innerText = pad.isPressed(index, button);
+            break;
+        case "Back":
+            document.getElementById("ButtonBackResult").innerText = pad.isPressed(index, button);
+            break;
+        case "LeftBumper":
+            document.getElementById("ButtonLBResult").innerText = pad.isPressed(index, button);
+            break;
+        case "RightBumper":
+            document.getElementById("ButtonRBResult").innerText = pad.isPressed(index, button);
+            break;
+        case "LeftClick":
+            document.getElementById("LClickResult").innerText = pad.isPressed(index, button);
+            break;
+        case "RightClick":
+            document.getElementById("RClickResult").innerText = pad.isPressed(index, button);
+            break;
+        case "DPadUp":
+            document.getElementById("UpResult").innerText = pad.isPressed(index, button);
+            break;
+        case "DPadDown":
+            document.getElementById("DownResult").innerText = pad.isPressed(index, button);
+            break;
+        case "DPadLeft":
+            document.getElementById("LeftResult").innerText = pad.isPressed(index, button);
+            break;
+        case "DPadRight":
+            document.getElementById("RightResult").innerText = pad.isPressed(index, button);
+            break;
+
+    }
+}
+
+/*while (1) {
+    btnPressed(0, "A");
+}*/

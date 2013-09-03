f5e799168cb5e145646c87a4cc5ca9deca8d7eec
Ryan D. Souders <honkeykong@honkeykong.org>
2013-09-03 11:17:15 -0500

    Added preliminary documentation in game.js

--- Xbox360PadTest/js/game.js
+++ Xbox360PadTest/js/game.js
@@ -4,18 +4,8 @@ var ctx = canvas.getContext('2d');
 canvas.width = screen.width;
 canvas.height = screen.height;
 
-var keysDown = {};
-
-var pad = Xbox360Pad.Pad;
-var numPads = pad.initControllers();
-
-window.addEventListener('keydown', function(e) {
-    keysDown[e.keyCode] = true;
-});
-
-window.addEventListener('keyup', function(e) {
-    delete keysDown[e.keyCode];
-});
+var pad = Xbox360Pad.Pad; // Create the controller object.
+var numPads = pad.initControllers(); // Initialize and return number of connected controllers.
 
 var mySprite = {
 	x: 200,
@@ -26,17 +16,32 @@ var mySprite = {
 	color: '#c00'
 }
 
+/* The controller functions return an array with the state of all the connected controllers. 
+In this case, we're grabbing index 0, which denotes player 1. 
+Obviously, indices 1, 2, and 3 denote players 2, 3 and 4.
+
+Valid options for pad.isPressed() are A, B, X, Y, DPadUp, DPadDown, DpadLeft, DPadRight,
+LeftClick, RightClick, LeftBumper, RightBumper, Back, and Start.
+
+The Left and Right analog triggers can be accessed with getLeftTrigger() and getRightTrigger(),
+respectively. Each returns a byte value between 0 and 255.
+
+To access the analog joystick axes, the functions getLStickX(), getLStickY(),
+getRStickX(), and getRStickY(). These functions return an unsigned short between
+-32767 and 32767. To compensate for jitter and old, worn-out joysticks, a deadzone
+threshold of 10000 is recommended. */
+
 function update(mod) {
-    if ((pad.isPressed("DPadLeft")[0]) || (pad.getLStickX()[0] <= -10000)) { //left
+    if ((pad.isPressed("DPadLeft")[0]) || (pad.getLStickX()[0] <= -10000)) { // Left
         mySprite.x -= mySprite.speed * mod; 
     }
-    if ((pad.isPressed("DPadUp")[0]) || (pad.getLStickY()[0] >= 10000)) { //up
+    if ((pad.isPressed("DPadUp")[0]) || (pad.getLStickY()[0] >= 10000)) { // Up
         mySprite.y -= mySprite.speed * mod;
     }
-    if ((pad.isPressed("DPadRight")[0]) || (pad.getLStickX()[0] >= 10000)) { //right
+    if ((pad.isPressed("DPadRight")[0]) || (pad.getLStickX()[0] >= 10000)) { // Right
         mySprite.x += mySprite.speed * mod;
     }
-    if ((pad.isPressed("DPadDown")[0]) || (pad.getLStickY()[0] <= -10000)) { //down
+    if ((pad.isPressed("DPadDown")[0]) || (pad.getLStickY()[0] <= -10000)) { // Down
         mySprite.y += mySprite.speed * mod;
     }
     if (pad.isPressed("A")[0]) { // A Button
